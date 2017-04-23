b65cf9217234e092885f6fb514515c34cf0fb02f
Michael Roschenwimmer <michael.roschenwimmer@gmail.com>
2017-04-23 17:12:26 -0600

    change

--- src/blackjackgame/BlackJackGameDriver.java
+++ src/blackjackgame/BlackJackGameDriver.java
@@ -9,62 +9,64 @@ import cardsanddeck_classes_rankandsuit_enums.Shuffler;
 public class BlackJackGameDriver {
 
 	BlackJackDealer dealerOne = new BlackJackDealer(); // whole bunch of
-														// instantiation
+	// instantiation
 	BlackJackPlayer playerOne = new BlackJackPlayer();
 	Shuffler shuffleOneDeckOnly = new Shuffler();
 	One52CardDeck one52CardDeck = new One52CardDeck();
-//	BlackJackHand playerOneHand = new BlackJackHand();
-//	BlackJackHand dealerOnehand = new BlackJackHand();
-	
-	
+	// BlackJackHand playerOneHand = new BlackJackHand();
+	// BlackJackHand dealerOnehand = new BlackJackHand();
+
 	// need to add more methods later
 
 	public void startBlackJackGame() {
-	 one52CardDeck.createOne52CardDeck(); 
-//	 shuffleOneDeckOnly.shuffleOneDeck(oneDeck)/ / created a 52 card deck
-//	shuffleOneDeckOnly.shuffleOneDeck(one52CardDeck);     // need help from Aaron here
-		
+		dealerOne.setName("Jimmy");
+		playerOne.setName("Player One");
+		one52CardDeck.createOne52CardDeck();
+		// shuffleOneDeckOnly.shuffleOneDeck(oneDeck)/ / created a 52 card deck
+		// shuffleOneDeckOnly.shuffleOneDeck(one52CardDeck); // need help from
+		// Aaron here
+
 		System.out.println("Dealer is dealing... Press Q to quit at any time...");
 		dealerDeals();
-		
-		if(dealerOne.playerHasBlackJack()) {
+
+		if (dealerOne.playerHasBlackJack()) {
 			System.out.println(dealerOne.getName() + " has BLACKJACK!");
-			if(playerOne.playerHasBlackJack()) {
+			if (playerOne.playerHasBlackJack()) {
 				System.out.println(dealerOne.getName() + " We both have BLACKJACK! Let's get to the next hand.");
-				dealerDeals(dealerOne);  // to loop to the next hand  --- PUSH
-			}
-			else if(playerOne.getHand().getHandValue() < 21) {
+				dealerDeals(); // to loop to the next hand --- PUSH
+			} else if (playerOne.getHand().getHandValue() < 21) {
 				System.out.println(dealerOne.getName() + " Sorry, you lose this hand " + playerOne.getName());
-				dealerDeals(dealerOne); // to loop to the next hand   -- insert method to take player's chip that they bet
-				
-		}
-		
-		if (playerOne.playerHasBlackJack()) {
-			System.out.println(playerOne.getName() + " has BLACKJACK!");
-			if(dealerOne.getHand().getHandValue() < 21) {
-				System.out.println("You win this hand " + playerOne.getName() + "!");
-				// insert method to add to player's chips
-				dealerDeals(dealerOne); // to loop to the next hand
+				dealerDeals(); // to loop to the next hand -- insert method to
+								// take player's chip that they bet
+
+			}
+
+			if (playerOne.playerHasBlackJack()) {
+				System.out.println(playerOne.getName() + " has BLACKJACK!");
+				if (dealerOne.getHand().getHandValue() < 21) {
+					System.out.println("You win this hand " + playerOne.getName() + "!");
+					// insert method to add to player's chips
+					dealerDeals(); // to loop to the next hand
+				}
 			}
 		}
-		}
-		
+
 	}
 
-	public void dealerDeals(BlackJackDealer robot) {
+	public void dealerDeals() {
 		Card dealtCard;
 
 		dealtCard = playerOne.hitMe(one52CardDeck.getOneCardFromDeck());
 		System.out.println(playerOne.getName() + "draws: " + dealtCard);
-		
-		dealtCard = robot.hitMe(one52CardDeck.getOneCardFromDeck());
-		System.out.println(robot.getName() + "draws: " + dealtCard);
-		
+
+		dealtCard = dealerOne.hitMe(one52CardDeck.getOneCardFromDeck());
+		System.out.println(dealerOne.getName() + "draws: " + dealtCard);
+
 		dealtCard = playerOne.hitMe(one52CardDeck.getOneCardFromDeck());
 		System.out.println(playerOne.getName() + "draws: " + dealtCard);
-		
-		dealtCard = robot.hitMe(one52CardDeck.getOneCardFromDeck());
-		System.out.println(robot.getName() + "draws their face-down card.");
+
+		dealtCard = dealerOne.hitMe(one52CardDeck.getOneCardFromDeck());
+		System.out.println(dealerOne.getName() + "draws their face-down card.");
 
 	}
 
@@ -77,14 +79,15 @@ public class BlackJackGameDriver {
 		Scanner kb = new Scanner(System.in);
 		String choice = kb.nextLine();
 
-		System.out.println(playerOne.getName() + " Would you like to hit or stay? Please type H for HIT or S for STAY.");
+		System.out
+				.println(playerOne.getName() + " Would you like to hit or stay? Please type H for HIT or S for STAY.");
 		if (choice.equals("H") || choice.equals("h")) {
 			System.out.println("So, you're feeling lucky, eh?");
 			// Insert HIT method here
 		}
 		if (choice.equals("S") || choice.equals("s")) {
 			// Insert STAY method here
-			
+
 		}
 		if (choice.equals("Q") || choice.equals("q")) {
 			System.out.println("Thanks for giving us your money! Come back again now, y'hear!?");
