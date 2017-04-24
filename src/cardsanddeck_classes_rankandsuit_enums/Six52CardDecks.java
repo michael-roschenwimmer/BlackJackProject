9cf1bd91cd540912daab92e07202b7140ae8e062
Michael Roschenwimmer <michael.roschenwimmer@gmail.com>
2017-04-23 23:59:06 -0600

    trying to get multi-deck working

--- src/cardsanddeck_classes_rankandsuit_enums/Six52CardDecks.java
+++ src/cardsanddeck_classes_rankandsuit_enums/Six52CardDecks.java
@@ -5,15 +5,23 @@ import java.util.List;
 
 public class Six52CardDecks {
 	
+	private ArrayList<Card> sixDecks = new ArrayList<>(312);
 	
 	public void createSix52CardDeck() {
-		List<Card> sixDecks = new ArrayList<>(312);
-
 		for (Suit suit : Suit.values()) {
 			for (Rank rank : Rank.values()) {
 				sixDecks.add(new Card(rank, suit));
 			}
 		}
+	}
+	public Card getOneCardFromSixDecks() {
+		return sixDecks.remove(0);
+	}
+	public ArrayList<Card> getSixDecks(){
+		return sixDecks;
+	}
+	public void setSixDecks(ArrayList<Card> sixDecks){
+		this.sixDecks = sixDecks;
 	}
 }
 
